fe2016b56b5493670fc9fcbcaeddbb08bc4c237e
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-08-10 19:53:33 +0530

    feat: Update min sdk version

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -1,6 +1,6 @@
 object AppConfig {
     const val COMPILE_SDK = 34
-    const val MIN_SDK = 24
+    const val MIN_SDK = 26
     const val TARGET_SDK = 34
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
